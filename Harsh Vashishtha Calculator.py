4030f67958623e90f2892e69eb83b1dd6a35cff2
Harsh Vashishtha <96041137+vashishtharsh@users.noreply.github.com>
2021-12-17 13:11:50 +0530

    Add files via upload

---
+++ Harsh Vashishtha Calculator.py
@@ -0,0 +1,22 @@
+while True:
+    print("Welcome to mini Calculator")
+    operator=input("\n select the operator(+,-,*,/,%) : ")
+    c1=int(input("\nenter the first number : "))
+    c2=int(input("enter the second number : "))
+    if operator=="+":
+        print("answer is ; ", f1+f2)
+
+    if operator=="+":
+        print("answer is ; ", f1-f2)
+
+    if operator=="+":
+        print("answer is ; ", f1*f2)
+
+    if operator=="+":
+        print("answer is ; ", f1/f2)
+    
+    if operator=="+":
+        print("answer is ; ", f1%f2)
+
+    else:
+        print("invalid operation")
