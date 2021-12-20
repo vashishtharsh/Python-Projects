979a14ffd8c5af8790fd3c5981cc568e59621216
Harsh Vashishtha <96041137+vashishtharsh@users.noreply.github.com>
2021-12-19 23:16:44 -0800

    Update Harsh Vashishtha Calculator.py

--- Harsh Vashishtha Calculator.py
+++ Harsh Vashishtha Calculator.py
@@ -1,24 +1,24 @@
 print("Welcome to mini Calculator")
 while True:
-    if ==1:
+    if ==1 :
         
     print("Welcome to mini Calculator")
-    opt=input("\n Select the operator you want(+,-,*,/,%) : ")
-    c1=int(input("\nenter the first number : "))
-    c2=int(input("enter the second number : "))
-    if operator=="+":
+    opt = input("\n select the operator(+,-,*,/,%) : ")
+    c1 = int(input("\nenter the first number : "))
+    c2 = int(input("enter the second number : "))
+    if opt=="+":
         print("answer is ; ", f1+f2)
 
-    if operator=="+":
+    if opt=="-":
         print("answer is ; ", f1-f2)
 
-    if operator=="+":
+    if opt=="*":
         print("answer is ; ", f1*f2)
 
-    if operator=="+":
+    if opt=="/":
         print("answer is ; ", f1/f2)
     
-    if operator=="+":
+    if opt=="%":
         print("answer is ; ", f1%f2)
 
     else:
