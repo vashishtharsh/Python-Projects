edec6978b321024a9bacf068cf4f5321686df5c1
Harsh Vashishtha <96041137+vashishtharsh@users.noreply.github.com>
2021-12-23 12:34:12 +0530

    Add files via upload

---
+++ Harsh Vashishtha  rows and columns.py
@@ -0,0 +1,12 @@
+a=int (input("Enter the number of rows"))
+b=int (input("Enter the number of columns"))
+while a== 'rows':
+      print (a)
+      a=i+1
+for i in range (5):
+    print (a)
+if (a == "1"):
+    print ("number of rows is :",  5)
+else:
+    print ("number of coloumns is :",  4)
+
