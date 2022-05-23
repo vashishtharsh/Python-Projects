8faa727ce30d91c6b792982e3ad8f1550b15ca9d
Harsh Vashishtha <96041137+vashishtharsh@users.noreply.github.com>
2022-05-23 14:30:32 +0530

    Add files via upload

---
+++ H V Bubble Sort.py
@@ -0,0 +1,24 @@
+def bubble_sort(array):
+    isSorted = False
+    passes = 0
+    length = len(array)
+    while not isSorted:
+        isSorted = True
+        # perform a pass through the array
+        # excluding already sorted positions
+        for i in range(length-1-passes):
+            if array[i] > array[i+1]:
+                swap(i, i+1, array)
+                # array is not sorted yet
+                isSorted = False
+        passes += 1
+    return array
+
+
+def swap(i, j, array):
+    # Swap values at indexes i and j
+    array[i], array[j] = array[j], array[i]
+
+
+arr = [1, 9, 5, 3, 7, 2, 6]
+print(bubble_sort(arr))  # [1, 2, 3, 5, 6, 7, 9]
